210781ffa01b74652014c9ce571a054dbc6671d2
zhujh2015 <zhujunhua0401@126.com>
2016-11-16 23:49:28 +0800

    add examples

---
+++ src/main/java/com/javase/junit/Test.java
@@ -0,0 +1,15 @@
+package com.junit;
+
+import java.util.Enumeration;
+
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Thread t;
+		Enumeration e;
+	}
+
+}
